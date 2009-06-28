7b0184c46d61c30631e8496429fcde7a58567cb6
horseluke@126.com <horseluke@users.noreply.github.com>
2009-06-28 16:33:53 +0000

    rev 54（2009-6-29）: 0.0.3 Build 20090629 Rev 54发布。该版本为国际版本（全英语提示）。 增加一个开关，自由选择是生成HTML文件还是log文件。（使用方法：打开debug.func.php，找到“define('LOG_TO_HTML',false);”。当为true时，则生成HTML文件；false则生成log文件） 生成的html代码符合绝大部分xhtml标准。感谢Freediscuz!的044003的辛勤劳动。

--- DiscuzSQLDebug_SimpleToHTMLLog/upload/include/debug.func.php
+++ DiscuzSQLDebug_SimpleToHTMLLog/upload/include/debug.func.php
@@ -1,6 +1,6 @@
 <?php 
 /**
-  Copyright 2009 Horse Luke（竹节虚）.
+  Copyright 2009 Horse Luke & 044003
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -14,106 +14,138 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 
-* Output the HTML debugging string to HtmlFile in color coded glory for a sql query 
+* Output the debugging string to File(HTML or LOG type)
 * For Discuz! SQLDEBUG ONLY!
 * @Special Thans to Daevid Vincent [daevid@LockdownNetworks.com] and renothing[FreeDiscuz!], 044003[FreeDiscuz!]
-* @version    0.0.2 Build 20090628 Rev 53 
-* @date       2009-6-28
-*/ 
-
+* @version    0.0.3 Build 20090629 Rev 54
+* @date       2009-6-29
+*/
 
 if(!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
 
-function sqldebug( $query ) 
-{ 
-   if( $query == '' ) return false; 
+//define whether output the logs to html file or not. if define it to false ,it will output to plain text(log suffix).  
+define('LOG_TO_HTML', true); 
+
+
+if(function_exists(date_default_timezone_set)) {
+    date_default_timezone_set("PRC");
+}
+
+
+
+//Function: Main SqlDebug Function
+function sqldebug($query)
+{
+   if($query == '') return false;
 
    global $SQL_INT; 
-   if( !isset($SQL_INT) ){
-	   //一个新的数据库请求周期产生了
-	   $SQL_INT = 0;
-	   echo "<div style=\"text-align:left;background-color:#FFC\"><b>注意：现在你已经打开了SQLDebug调试（简单写入日志版本），请随时留意并自行删除过时的SQLDebug HTML日志（位于文件夹/forumdata/logs/下）。</b></div>";
-	   
-	   //往日志内写入当前的url
-	   $CurrentURL = htmlentities("http://{$_SERVER['SERVER_NAME']}:{$_SERVER['SERVER_PORT']}{$_SERVER['REQUEST_URI']}");
-	   $str =  "\n\n<div><br />-----------------------------------------------------------<br /><b>URL:</b>{$CurrentURL}</div>";
-	   sqlDebugWrite($str);
+   if(!isset($SQL_INT)) {
+		//A new database request generated
+		$SQL_INT = 0;
+        $fileName = getSqlDebugLogFileName();
+		echo "<div style=\"text-align:left;background-color:#FFC;font-weight:bold;color:#000\">Note: SQLDebug(SimpleToHTMLLog) Mode is ON. Current log filename: <span style=\"color:#FF6600;font-weight:bold\">{$fileName}</span><br />Please keep an eye on the SQLDebug log files which are located in the folder '/forumdata/logs/' , and delete the outdated files when necessary.</div>";
+
+		//Write the current url to log
+		$currentURL = "http://{$_SERVER['SERVER_NAME']}:{$_SERVER['SERVER_PORT']}{$_SERVER['REQUEST_URI']}";
+		if(true === LOG_TO_HTML){
+            $currentURL =  "\n\n<div><br />-----------------------------------------------------------<br /><b>URL:</b>".htmlentities($currentURL)."</div>";
+		}else{
+            $currentURL = "\n\n-----------------------------------------------------------\nURL: {$currentURL}\n";
+		}
+		sqlDebugWrite($currentURL);
    }
 
-   //[dv] this has to come first or you will have goofy results later. 
-   $query = preg_replace("/['\"]([^'\"]*)['\"]/i", "'<FONT COLOR='#FF6600'>$1</FONT>'", $query, -1); 
 
-   $query = str_ireplace( 
-                           array ( 
-                                   '*', 
-                                   'SELECT ', 
-                                   'UPDATE ', 
-                                   'DELETE ', 
-                                   'INSERT ', 
-                                   'INTO', 
-                                   'VALUES', 
-                                   'FROM', 
-                                   'LEFT', 
-                                   'JOIN', 
-                                   'WHERE', 
-                                   'LIMIT', 
-                                   'ORDER BY', 
-                                   'AND', 
-                                   'OR ', //[dv] note the space. otherwise you match to 'COLOR' ;-) 
-                                   'DESC', 
-                                   'ASC', 
-                                   'ON ' 
-                                 ), 
-                           array ( 
-                                   "<FONT COLOR='#FF6600'><B>*</B></FONT>", 
-                                   "<FONT COLOR='#00AA00'><B>SELECT</B> </FONT>", 
-                                   "<FONT COLOR='#00AA00'><B>UPDATE</B> </FONT>", 
-                                   "<FONT COLOR='#00AA00'><B>DELETE</B> </FONT>", 
-                                   "<FONT COLOR='#00AA00'><B>INSERT</B> </FONT>", 
-                                   "<FONT COLOR='#00AA00'><B>INTO</B></FONT>", 
-                                   "<FONT COLOR='#00AA00'><B>VALUES</B></FONT>", 
-                                   "<FONT COLOR='#00AA00'><B>FROM</B></FONT>", 
-                                   "<FONT COLOR='#00CC00'><B>LEFT</B></FONT>", 
-                                   "<FONT COLOR='#00CC00'><B>JOIN</B></FONT>", 
-                                   "<FONT COLOR='#00AA00'><B>WHERE</B></FONT>", 
-                                   "<FONT COLOR='#AA0000'><B>LIMIT</B></FONT>", 
-                                   "<FONT COLOR='#00AA00'><B>ORDER BY</B></FONT>", 
-                                   "<FONT COLOR='#0000AA'><B>AND</B></FONT>", 
-                                   "<FONT COLOR='#0000AA'><B>OR</B> </FONT>", 
-                                   "<FONT COLOR='#0000AA'><B>DESC</B></FONT>", 
-                                   "<FONT COLOR='#0000AA'><B>ASC</B></FONT>", 
-                                   "<FONT COLOR='#00DD00'><B>ON</B> </FONT>" 
-                                 ), 
-                           $query 
-                         );
+	$time = date("Y-m-d h:i:s A");
 
-   if(function_exists(date_default_timezone_set)){
-        date_default_timezone_set("PRC");
-   }
+	if(true === LOG_TO_HTML){
+        $query = sqlStringToHTML($query);
+	    $query = "<div><span style=\"color:#0000FF;font-weight:bold\">{$time} - SQL[{$SQL_INT}]:</span> {$query}<span style=\"color:#FF0000\">;</span></div>\n";
+	}else{
+	    $query = "{$time} - SQL[{$SQL_INT}]: {$query};\n";
+	}
 
-   $time = date("Y-m-d h:i:s A");
+	sqlDebugWrite($query);
 
-   $query = "<div><FONT COLOR='#0000FF'><B>{$time} - SQL[".$SQL_INT."]:</B> ".$query."<FONT COLOR='#FF0000'>;</FONT></FONT></div>\n";
-   sqlDebugWrite($query);
+	$SQL_INT++;
 
-   $SQL_INT++;
-
-} //SQL_DEBUG 
+} //SQL_DEBUG
 
 
 
-function sqlDebugWrite($str){
-    $fileName = date('Y-m-d').'-SQLDebug.html';
+//Function: Convert a sql query string to the color coded glory
+function sqlStringToHTML($query){
+	$query = preg_replace("/['\"]([^'\"]*)['\"]/i", "'<span style=\"color:#FF6600\">$1</span>'", $query, -1); 
+
+	$query = str_ireplace(
+		array (
+			'*',
+			'SELECT ',
+			'UPDATE ',
+			'DELETE ',
+			'INSERT ',
+			'INTO',
+			'VALUES',
+			'FROM',
+			'LEFT', 
+			'JOIN',
+			'WHERE',
+			'LIMIT',
+			'ORDER BY',
+			'AND',
+			'OR ',
+			'DESC',
+			'ASC',
+			'ON '
+		),
+		array (
+			"<span style=\"color:#FF6600;font-weight:bold\">*</span>",
+			"<span style=\"color:#00AA00;font-weight:bold\">SELECT </span>",
+			"<span style=\"color:#00AA00;font-weight:bold\">UPDATE </span>",
+			"<span style=\"color:#00AA00;font-weight:bold\">DELETE </span>", 
+			"<span style=\"color:#00AA00;font-weight:bold\">INSERT </span>",
+			"<span style=\"color:#00AA00;font-weight:bold\">INTO</span>",
+			"<span style=\"color:#00AA00;font-weight:bold\">VALUES</span>",
+			"<span style=\"color:#00AA00;font-weight:bold\">FROM</span>",
+			"<span style=\"color:#00CC00;font-weight:bold\">LEFT</span>",
+			"<span style=\"color:#00CC00;font-weight:bold\">JOIN</span>", 
+			"<span style=\"color:#00AA00;font-weight:bold\">WHERE</span>",
+			"<span style=\"color:#AA0000;font-weight:bold\">LIMIT</span>",
+			"<span style=\"color:#00AA00;font-weight:bold\">ORDER BY</span>",
+			"<span style=\"color:#0000AA;font-weight:bold\">AND</span>",
+			"<span style=\"color:#0000AA;font-weight:bold\">OR </span>",
+			"<span style=\"color:#0000AA;font-weight:bold\">DESC</span>",
+			"<span style=\"color:#0000AA;font-weight:bold\">ASC</span>",
+			"<span style=\"color:#00DD00;font-weight:bold\">ON </span>"
+		),
+		$query
+	);
+	return $query;
+}
+
+
+//Function: Write logs to file
+function sqlDebugWrite($str) {
+	$fileName = getSqlDebugLogFileName();
 	$logDir = DISCUZ_ROOT.'/forumdata/logs/';
-	
+
 	$fileHandle = @fopen("{$logDir}{$fileName}",'a');
-    @fwrite($fileHandle,$str);
-    @fclose($fileHandle);
+	@fwrite($fileHandle,$str);
+	@fclose($fileHandle);
 
 }
 
 
+//Function: define and return the log filename
+function getSqlDebugLogFileName(){
+    if(true === LOG_TO_HTML){
+        $fileName = date('Y-m-d').'-SQLDebug.html';
+	}else{
+        $fileName = date('Y-m-d').'-SQLDebug.log';
+	}
+	return $fileName;
+}
 
 ?>
