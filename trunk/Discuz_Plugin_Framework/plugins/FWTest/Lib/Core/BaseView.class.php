410078d286ef891a33627137fb11b4e689a54f15
horseluke@126.com <horseluke@126.com@c125e93a-fd63-11dd-802b-177316e4ac06>
2009-07-26 14:04:43 +0000

    Discuz 插件自写框架-未完工代码。增加db工厂模式。

--- trunk/Discuz_Plugin_Framework/plugins/FWTest/Lib/Core/BaseView.class.php
+++ trunk/Discuz_Plugin_Framework/plugins/FWTest/Lib/Core/BaseView.class.php
@@ -7,41 +7,39 @@
  * @version ver 0.0.1 build 20090724 rev 1 For Discuz! 7
  */
 
-!defined('IN_DZ_PLUGIN_FW') && exit();
+!defined('IN_FW') && exit();
 
 class BaseView{
-	public $t_var = array();
-	
-	public function assign($name,$value=''){
-		if(is_array($name)){
-			foreach ($name as $k => $v){
-				//$k = strval($k);
-				$this->t_var[$k] = $v;
-			}
-		}else{
-			//$name = strval($name);
-		    $this->t_var[$name] = $value;
-		}
-	}
-	
-	public function display($tplFileName=''){
-		$controller = FWBase::getConfigValue('DEFAULT_CONTROLLER').'Controller';
-		if (empty($tplFileName)){
-		    $tplFileName = FWBase::getConfigValue('DEFAULT_ACTION').'Action';
-		}
-		$tplFilePath = APP_PATH."/Tpl/{$controller}/{$tplFileName}.htm";
-		if(!is_file($tplFilePath)){
-				$map['message']='未找到模版！无法显示结果！';
-				$map['type'] = 'FRAMEWORK_ERROR';
-				FWBase::throw_exception($map);
-		}
+    public $t_var = array();
+    
+    public function assign($name,$value=''){
+        if(is_array($name)){
+            foreach ($name as $k => $v){
+                //$k = strval($k);
+                $this->t_var[$k] = $v;
+            }
+        }else{
+            //$name = strval($name);
+            $this->t_var[$name] = $value;
+        }
+    }
+    
+    public function display($tplFileName=''){
+        $controller = FWBase::getConfig('DEFAULT_CONTROLLER').'Controller';
+        if (empty($tplFileName)){
+            $tplFileName = FWBase::getConfig('DEFAULT_ACTION').'Action';
+        }
+        $tplFilePath = APP_PATH."/Tpl/{$controller}/{$tplFileName}.htm";
+        if(!is_file($tplFilePath)){
+                FWBase::throw_exception('未找到模版！无法显示结果！','FRAMEWORK_ERROR');
+        }
 
-		foreach ($this->t_var as $name => $value){
-			$$name = $value;
-		}
-		//ob_start();
-		require ($tplFilePath);
-		//ob_end_flush();
-		//exit;
-	}
+        foreach ($this->t_var as $name => $value){
+            $$name = $value;
+        }
+        //ob_start();
+        require ($tplFilePath);
+        //ob_end_flush();
+        //exit;
+    }
 }
