be7964cc0f44fd21138d64465bdaf81019227a8d
horseluke@126.com <horseluke@users.noreply.github.com>
2009-11-07 16:15:25 +0000

    用户帖子信息列表for 7.1(开发中): 基本成型-3 功能演示～php核心代码准备展开

--- iirs_userPostList/7.1/plugins/iirs_userPostList/App/Controller/DiscuzUserController.php
+++ iirs_userPostList/7.1/plugins/iirs_userPostList/App/Controller/DiscuzUserController.php
@@ -3,28 +3,43 @@ if(!defined('IN_DISCUZ')) {
     exit('Access Denied');
 }
 
-class UserController extends BaseController{
+class DiscuzUserController extends BaseController{
 
     public function __construct(){
-        $this->_getParamFromUrl();
         parent::__construct();
-    }
-    
-    public function getThreadlistAction(){
-        showmessage("getThreadlistAction: <a href=\"search.php?srchuid={$this->_param['uid']}&amp;srchfid=all&amp;srchfrom=0&amp;searchsubmit=yes\" target=\"_blank\">点击这里搜索他发布的帖子</a>", NULL,  'HALTED');
+        $this->_getParamFromUrl();
     }
     
     public function getPostlistAction(){
         //showmessage("getPostlistAction: 成功接收到uid值：{$this->_param['uid']}。请继续代码编写。", NULL,  'HALTED');
-        $this->assign('uid',$this->_param['uid']);
-        $this->display('UserController_getPostlistAction');
+        require_once(APP_PATH.'/Model/DiscuzUserModel.php');
+        $targetUser = new DiscuzUserModel($this->_param['uid']);    //被操作的目标用户实例
+        $currentUser = new DiscuzUserModel($GLOBALS['discuz_uid']);      //当前用户实例
+
+        //$postListResult = $targetUser->getPostlist(0,20,$currentUser->getDisallowVisitFidList());
+
+        //$this->assign('floatTitle',"{$targetUser->username}的回复（<a href=\"search.php?srchuid={$targetUser->uid}&amp;srchfid=all&amp;srchfrom=0&amp;searchsubmit=yes\" target=\"_blank\">点击这里搜索他发布的帖子</a>）");
+        $this->assign('uid',$targetUser->uid);
+        $this->assign('username',$targetUser->username);
+        $this->display('DiscuzUserController_getPostlistAction');
+    }
+    
+    
+    public function getAttachmentlistAction(){
+        require_once(APP_PATH.'/Model/DiscuzUserModel.php');
+        $targetUser = new DiscuzUserModel($this->_param['uid']);    //被操作的目标用户实例
+        $currentUser = new DiscuzUserModel($GLOBALS['discuz_uid']);      //当前用户实例
+        
+        $this->assign('uid',$targetUser->uid);
+        $this->assign('username',$targetUser->username);
+        $this->display('DiscuzUserController_getAttachmentlistAction');
     }
     
     
     private function _getParamFromUrl(){
         //安全过滤URL传值代码
         if( empty($GLOBALS['uid']) || !is_numeric($GLOBALS['uid']) || ($GLOBALS['uid'] < 1) ||$GLOBALS['uid'] == $GLOBALS['discuz_uid'] ){
-            showmessage('<b><a href="my.php?item=threads" target="_blank">要查看自己发布的帖子，请点击这里进入“个人中心”的“我的帖子”查看。</a></b>', NULL,  'HALTED');
+            showmessage('<b><a href="my.php?item=posts" target="_blank">要查看自己发布的帖子，请点击这里进入“个人中心”的“我的帖子”查看。</a></b>', NULL,  'HALTED');
             exit();
         }else{
             $this->_param['uid'] = abs(intval($GLOBALS['uid']));
--- iirs_userPostList/7.1/plugins/iirs_userPostList/App/Model/DiscuzUserModel.php
+++ iirs_userPostList/7.1/plugins/iirs_userPostList/App/Model/DiscuzUserModel.php
@@ -3,7 +3,7 @@ if(!defined('IN_DISCUZ')) {
     exit('Access Denied');
 }
 
-class UserModel extends BaseModel{
+class DiscuzUserModel extends BaseModel{
     
     public $uid;
     public $username;
@@ -11,17 +11,35 @@ class UserModel extends BaseModel{
     public $adminid;
     protected $disallowVisitFidList;
     
+    /**
+     * Construct an instance of user
+     *
+     * @param numeric $uid 用户的uid值
+     */
     public function __construct($uid){
-        
+        parent::__construct();
+        if($uid == $GLOBALS['discuz_uid']){
+            $this->uid = $GLOBALS['discuz_uid'];
+            $this->username = $GLOBALS['discuz_user'];
+            $this->groupid = $GLOBALS['groupid'];
+            $this->adminid = $GLOBALS['adminid'];
+        }else{
+            $member = $this->db->fetch_first("SELECT uid,username,groupid,adminid FROM {$GLOBALS['tablepre']}members WHERE uid = '{$uid}'");
+            if(!is_array($member)){
+                showmessage('不存在此用户，请返回。',null,'HALTED');
+            }else{
+                $this->uid = $member['uid'];
+                $this->username = $member['username'];
+                $this->groupid = $member['groupid'];
+                $this->adminid = $member['adminid'];
+            }
+        }
     }
     
     public function getDisallowVisitFidList(){
         
     }
-    
-    public function getThreadlist($startnum,$limitnum,$ignoreFidList=array()){
-        
-    }
+
     
     public function getPostlist($startnum,$limitnum,$ignoreFidList=array()){
         
